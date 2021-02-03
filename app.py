e3f4ce172d053f0b04e0c48f99fa80d92da9ef91
Your Name <you@example.com>
2021-02-02 23:37:39 -0500

    Completed part 1,2,3

--- app.py
+++ app.py
@@ -14,12 +14,11 @@ app = Flask(__name__)
 # Put your routes below this comment
 '''
 
+@app.route('/')
+def home():
+    return 'My Home Page'
 
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port=8080)
